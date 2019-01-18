72332573c44f26d17f5be3c460168307e89bb5ee
Tim Davies <timdavies@me.com>
2019-01-18 15:11:25 +0000

    Update tests

--- tests/search_test.go
+++ tests/search_test.go
@@ -15,13 +15,13 @@ func BenchmarkSearch(b *testing.B) {
 
 	str := "👍🏻"
 	for i := 0; i < b.N; i++ {
-		emoji.DetectEmoji(str)
+		emoji.FindAll(str)
 	}
 }
 
 func BenchmarkComplexSearch(b *testing.B) {
 	str := "This is a string 😄 🐷 with some 👍🏻🙈 emoji! 🐷 🏃🏿‍♂️"
 	for i := 0; i < b.N; i++ {
-		emoji.DetectEmoji(str)
+		emoji.FindAll(str)
 	}
 }
